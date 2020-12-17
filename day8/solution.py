62b2d57e0df0cc9982d18bf68ac5473383e51d08
TheSquareOne <samppa.valko@gmail.com>
2020-12-17 22:03:09 +0200

    Day 8 solution for part 1 and 2

--- day8/solution.py
+++ day8/solution.py
@@ -7,7 +7,7 @@ def readCode(filename):
     return bootCode
 
 
-def findLoop(bootCode):
+def getAcc(bootCode):
 
     acc = 0
     i = 0
@@ -21,7 +21,7 @@ def findLoop(bootCode):
         if i not in usedInst:
             usedInst.append(i)
         else:
-            return acc
+            return -1, acc
 
         # Commands
         if cmd == 'acc':
@@ -34,8 +34,36 @@ def findLoop(bootCode):
 
         i += 1
 
+    return 0, acc
+
+
+def fixBootCode(bootCode):
+    i = 0
+    while i < len(bootCode):
+
+        bootCode_copy = bootCode.copy()
+        cmd, arg = bootCode_copy[i].strip().split(' ')
+
+        if cmd == 'jmp':
+            bootCode_copy[i] = bootCode_copy[i].replace('jmp', 'nop')
+        elif cmd == 'nop' and arg != 0:
+            bootCode_copy[i] = bootCode_copy[i].replace('nop', 'jmp')
+
+        i += 1
+
+        err, acc = getAcc(bootCode_copy)
+
+        if err == 0:
+            print('Instruction number', i, 'was causing the loop!')
+            return acc
+
 
 bootCode = readCode('boot_code.txt')
 
 # Part 1
-print('Part 1 acc is',findLoop(bootCode))
+err, acc = getAcc(bootCode)
+print('Part 1 acc is', acc)
+
+# Part 2
+acc = fixBootCode(bootCode)
+print('Part 2 acc is', acc)
