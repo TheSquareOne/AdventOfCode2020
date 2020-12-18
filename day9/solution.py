9910169c7ffe9a4e8ea239e51e953609d98f98c2
TheSquareOne <samppa.valko@gmail.com>
2020-12-18 21:10:35 +0200

    Day 9 solution for part 1 and 2

--- day9/solution.py
+++ day9/solution.py
@@ -15,10 +15,32 @@ def findWeakness(data):
 
         if int(data[i]) not in map(sum, combinations(preamble, 2)):
             print('Weakness found in line', i + 1, ',', data[i])
-            return data[i]
+            return int(data[i])
+
+
+def findContiguousSet(data, invalidNum):
+    s = []
+    for i in range(0, len(data) - 1):
+        for y in range(i, len(data)):
+            s.append(int(data[y]))
+
+            if sum(s) > int(invalidNum):
+                s.clear()
+                break
+            elif sum(s) == int(invalidNum):
+                print('Contiguous set for invalid number found from line',
+                    i, 'to', y)
+                print('Highest number being:', max(s))
+                print('Smallest number being:', min(s))
+                print('Addition of highest and smallest is:',
+                    max(s) + min(s))
+                return min(s) + max(s)
 
 
 data = readData('puzzle_input.txt')
 
 # Part 1
-findWeakness(data)
+invalidNum = findWeakness(data)
+
+# Part 2
+invalidSum = findContiguousSet(data, invalidNum)
